9bde3f1a21062c15c90b3fa48d2fb1d125338d86
SleepDeprivedVFX <AdamBenson.vfx@gmail.com>
2024-10-14 17:41:46 -0700

    v1.3.10: Updating the documentation. Lots of commit/pushes here.

--- ui/ui_superSaver_UI.py
+++ ui/ui_superSaver_UI.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'superSaver_UIEqGsRo.ui'
+## Form generated from reading UI file 'superSaver_UIpnPpMY.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.7.1
 ##
